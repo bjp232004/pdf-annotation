5a7eadf16bd49499782b0ddfad61d76222406f76
Bhargav <bhargav.j.patel@accenture.com>
2016-07-22 19:35:25 +0530

    Commit for changes

--- pdf-annotation.js
+++ pdf-annotation.js
@@ -340,6 +340,311 @@
         }
       }
     }
+    
+    factoryObj.event = {
+      options: {
+        activeTool: ''
+      },
+      init: function(canvas, ctx) {
+        this.canvas = canvas;
+        this.canvas_coords = this.canvas.getBoundingClientRect();
+        this.ctx = ctx;
+        this.ctx.strokeColor = this.options.stroke_color;
+        this.paging();
+        this.addCanvasEvents();
+      },
+      addCanvasEvents: function() {
+        this.canvas.addEventListener('mousedown', this.start.bind(this));
+        this.canvas.addEventListener('mousemove', this.stroke.bind(this));
+        this.canvas.addEventListener('mouseup', this.stop.bind(this));
+        this.canvas.addEventListener('mouseout', this.stop.bind(this));
+      },
+      start: function(evt) {
+        if(factoryObj.history.raw_undo_list[factoryObj.history.options.activePage].length > 0) {
+          factoryObj.move.init(this.canvas, this.ctx);
+          factoryObj.move.start(evt);
+        }
+
+        if(factoryObj.move.options.movedObject == -1) {
+          if(this.options.activeTool !== '' && this.options.activeTool !== undefined) {
+            this.options.activeTool.start(evt);
+          }
+        }
+
+      },
+      stroke: function(evt) {
+        if(this.options.activeTool !== '' && this.options.activeTool !== undefined && this.options.activeTool.drawing === true) {
+          this.options.activeTool.stroke(evt);
+        }
+      },
+      stop: function(evt) {
+        if(this.options.activeTool !== '' && this.options.activeTool !== undefined && this.options.activeTool.drawing === true) {
+          this.options.activeTool.stop(evt);
+        }
+      },
+      closepdf: function() {
+        factoryObj.options.closeFn();
+      },
+      savepdf: function() {
+          
+        var m = confirm("Are you sure to you want to save file?");
+        if (m) {
+          var doc = new jsPDF('p', 'mm', 'a4');
+          for(i = 0; i < parseInt(factoryObj.history.options.totalPage); i++) {
+            if(i<2){
+              if(factoryObj.history.final_canvas_url.hasOwnProperty(i)) {
+                var imgData = factoryObj.history.final_canvas_url[i];
+              }
+              else {
+                var imgData = document.getElementById("page"+i).toDataURL("image/png");
+              }
+              doc.addImage(imgData, 'PNG', 0, 0, 200, 250, null, 'SLOW');
+              if(i<parseInt(factoryObj.history.options.totalPage-1)){
+                doc.addPage();
+              }
+            }
+          }
+
+          var dataurl = doc.output('datauristring');
+          var blob = this.dataURLtoBlob(dataurl);
+
+          if(typeof(factoryObj.options.callbackFn) == "function") {
+            factoryObj.options.callbackFn({blob: blob});
+          } else {
+            doc.save();
+          }
+        }
+      },
+      dataURLtoBlob: function(dataurl) {
+        var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+            bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+        while(n--){
+            u8arr[n] = bstr.charCodeAt(n);
+        }
+        return new Blob([u8arr], {type:mime});
+    },
+      paging: function() {
+        factoryObj.history.options.totalPage = factoryObj.history.options.pdfobj.transport.numPages;
+        factoryObj.options.toolsObj.activePage.textContent = factoryObj.history.options.activePage + 1;
+        factoryObj.options.toolsObj.currentPage.value = factoryObj.history.options.activePage + 1;
+        factoryObj.options.toolsObj.totalPage.textContent = factoryObj.history.options.totalPage;
+        if(factoryObj.history.options.activePage === 0) {
+          factoryObj.options.toolsObj.prevBtn.setAttribute('disabled', 'disabled');
+          factoryObj.options.toolsObj.nextBtn.removeAttribute('disabled');
+        }
+        else if(parseInt(factoryObj.history.options.activePage) === parseInt(factoryObj.history.options.totalPage - 1)) {
+          factoryObj.options.toolsObj.nextBtn.setAttribute('disabled', 'disabled');
+          factoryObj.options.toolsObj.prevBtn.removeAttribute('disabled');
+        }
+        else if(factoryObj.history.options.activePage > 0 && parseInt(factoryObj.history.options.activePage) < parseInt(factoryObj.history.options.totalPage - 1)) {
+          factoryObj.options.toolsObj.prevBtn.removeAttribute('disabled');
+          factoryObj.options.toolsObj.nextBtn.removeAttribute('disabled');
+        }
+      }
+    }
+
+    factoryObj.text = {
+      name: 'text',
+      options: {
+        stroke_color: ['00', '00', '00'],
+        fill_color: '#FF0000',
+        font: '18px Arial',
+        dim: 4,
+        lineHeight: 14,
+        finalTextInfo: []
+      },
+      init: function(canvas, ctx) {
+        this.canvas = canvas;
+        this.ctx = ctx;
+        this.ctx.strokeColor = this.options.stroke_color;
+        this.ctx.fillStyle = 'rgba(0,0,0,0)';
+        this.drawing = false;
+        this.options.flag = true;
+      },
+      start: function(evt) {
+        if(!this.options.flag) return;
+        this.canvas_coords = this.canvas.getBoundingClientRect();
+        var x = evt.pageX - this.canvas_coords.left;
+        var y = evt.pageY - this.canvas_coords.top;
+
+        this.options.startX = x;
+        this.options.startY = y;
+
+        this.options.endX = x;
+        this.options.endY = y;
+
+        this.ctx.fillStyle = this.options.fill_color;
+        this.ctx.font = this.options.font;
+
+        this.ctx.beginPath();
+        this.drawing = true;
+      },
+      stroke: function(evt) {
+        if(!this.options.flag) return;
+        if(this.drawing) {
+          var x = evt.pageX - this.canvas_coords.left;
+          var y = evt.pageY - this.canvas_coords.top;
+          this.options.endX = x;
+          this.options.endY = y;
+        }
+      },
+      stop: function(evt) {
+        if(this.drawing && this.options.flag && this.options.startX !== this.options.endX && this.options.startY !== this.options.endY) {
+          this.drawing = false;
+          this.options.flag = false;
+
+          this.options.lineHeight = parseInt(factoryObj.history.options.font_size);
+
+          factoryObj.options.toolsObj.editor_wrapper.style.display = 'block';
+          factoryObj.options.toolsObj.editor_wrapper.style.top = this.options.startY + this.canvas_coords.top - 10;
+          factoryObj.options.toolsObj.editor_wrapper.style.left = this.options.startX + this.canvas_coords.left;
+          factoryObj.options.toolsObj.editor_wrapper.style.width = this.options.endX - this.options.startX;
+
+          factoryObj.options.toolsObj.contenteditor.focus();
+          factoryObj.options.toolsObj.contenteditor.style.minHeight = parseInt(this.options.endY - this.options.startY);
+        } else {
+          this.ctx.closePath();
+          this.ctx.stroke();
+          this.drawing = false;
+        }
+      },
+      drawTool: function() {
+        factoryObj.history.setStyleElement(this.canvas, this.ctx);
+
+        factoryObj.options.toolsObj.contenteditor.style.fontSize = factoryObj.history.options.font_size;
+        factoryObj.options.toolsObj.contenteditor.style.lineHeight = factoryObj.history.options.font_size+'px';
+        factoryObj.options.toolsObj.contenteditor.style.height = (factoryObj.options.toolsObj.contenteditor.scrollHeight)+"px";
+
+        var contenttext = factoryObj.options.toolsObj.contenteditor.value;
+        var enteredTextEncoded = escape(contenttext);
+        contenttext = unescape(enteredTextEncoded.replace(/%0A/g, '<br />').replace(/%20/g, ' '));
+        factoryObj.options.toolsObj.contenteditor.value = '';
+        if(contenttext !== '') {
+          var textObj = {
+            text:contenttext,
+            x: this.options.startX,
+            y: this.options.startY,
+            boxWidth: this.options.endX - this.options.startX
+          };
+
+          this.drawStyledText(textObj);
+          this.options.flag = true;
+        }
+
+      },
+      drawStyledText: function (textInfo) {
+        this.options.textInfo = textInfo;
+        var text = textInfo.text, x = textInfo.x, y = textInfo.y;
+        var splittedText, xAux, textLines = [], boxWidth = textInfo.boxWidth;
+        var proText, k, n, m;
+
+        var match = text.match(/<\s*br\s*\/>|<\s*class=["|']([^"|']+)["|']\s*\>([^>]+)<\s*\/class\s*\>|<\s*style=["|']([^"|']+)["|']\s*\>([^>]+)<\s*\/style\s*\>|[^<]+/g);
+        
+        for (m = 0; m < match.length; m++) {
+
+          if (/<\s*br\s*\/>/i.test(match[m])) {
+            y += parseInt(this.options.lineHeight, 10) * 1.5;
+            x = textInfo.x;
+            continue;
+          } else {
+            proText = match[m];
+          }
+
+          textLines = [];
+          proText = proText.replace(/\s*\n\s*/g, " ");
+
+          if (boxWidth !== undefined) {
+
+            if (this.checkLineBreak(proText, (boxWidth+textInfo.x), x)) {
+              splittedText = this.trim(proText).split(" ");
+
+              if (splittedText.length == 1) {
+                textLines.push({text: this.trim(proText) + " ", linebreak: true});
+              } else {
+                xAux = x;
+                var line = 0;
+                textLines[line] = {text: undefined, linebreak: false};
+
+                for (k = 0; k < splittedText.length; k++) {
+                  splittedText[k] += " ";
+                  if (!this.checkLineBreak(splittedText[k], (boxWidth+textInfo.x), xAux)) {
+                    if (textLines[line].text == undefined) {
+                      textLines[line].text = splittedText[k];
+                    } else {
+                      textLines[line].text += splittedText[k];
+                    }
+
+                    xAux += this.ctx.measureText(splittedText[k]).width;
+                  } else {
+                    xAux = textInfo.x;
+                    if (textLines[line].text !== undefined) {
+                      line++;
+                    }
+
+                    textLines[line] = {text: splittedText[k], linebreak: true};
+                    xAux += this.ctx.measureText(splittedText[k]).width;
+                  }
+                }
+              }
+            }
+          }
+
+          if (textLines.length == 0) {
+            textLines.push({text: this.trim(proText) + " ", linebreak: false});
+          }
+
+          for (n = 0; n < textLines.length; n++) {
+            if (textLines[n].linebreak) {
+              y += parseInt(this.options.lineHeight, 10);
+              x = textInfo.x;
+
+              this.options.endY = y
+            }
+            this.ctx.fillText(textLines[n].text, x, y);
+            this.options.finalTextInfo.push({'text': textLines[n].text, 'x': x, 'y': y});
+            x += this.ctx.measureText(textLines[n].text).width;
+          }
+        }
+
+        factoryObj.square.canvas = this.canvas;
+        factoryObj.square.ctx = this.ctx;
+        factoryObj.square.options.startX = this.options.startX-10;
+        factoryObj.square.options.startY = this.options.startY-15;
+        factoryObj.square.options.endX = this.options.endX;
+        factoryObj.square.options.endY = this.options.endY+10;
+        factoryObj.square.drawTool();
+
+        this.ctx.stroke();
+        factoryObj.history.setRawData();
+        factoryObj.history.saveState(this.canvas);
+      },
+      redrawTool: function(obj, diffX, diffY) {
+        for(var k=0; k < obj.length; k++) {
+          this.ctx.fillText(obj[k].text, obj[k].x+diffX, obj[k].y+diffY);
+        }
+
+        factoryObj.square.canvas = this.canvas;
+        factoryObj.square.ctx = this.ctx;
+        factoryObj.square.options.startX = this.options.startX-10;
+        factoryObj.square.options.startY = this.options.startY-15;
+        factoryObj.square.options.endX = this.options.endX;
+        factoryObj.square.options.endY = this.options.endY+10;
+        factoryObj.square.drawTool();
+      },
+      checkLineBreak: function (text, boxWidth, x) {
+        return (this.ctx.measureText(text).width + x > boxWidth);
+      },
+      trim: function (str) {
+        var ws, i;
+        str = str.replace(/^\s\s*/, '');
+        ws = /\s/;
+        i = str.length;
+        while (ws.test(str.charAt(--i))) {
+          continue;
+        }
+        return str.slice(0, i + 1);
+      }
+    };
 
     return factoryObj;
   });
