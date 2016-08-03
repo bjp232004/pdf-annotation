8ea7d21cdf043465a57a5308ce734ecfac6cacd0
Bhargav <bhargav.j.patel@accenture.com>
2016-08-03 14:11:10 +0530

    Commit for gulp generate folder

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,15 @@ var sprity = require('sprity');
 var uglify = require('gulp-uglify');
 var pump = require('pump');
 var babel = require('gulp-babel');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
 
 // generate sprite.png and _sprite.scss 
 gulp.task('sprites', function () {
     return sprity.src({
-        src: './src/images/**/*.{png,jpg}',
-        style: './sprite.css',
+        src: './src/images/**/*.{png,jpg}'
     })
-        .pipe(gulpif('*.png', gulp.dest('./dist/images/'), gulp.dest('./dist/css/')))
+        .pipe(gulpif('*.png', gulp.dest('./dist/images/')))
 });
 
 gulp.task('compress', function (cb) {
@@ -21,6 +22,16 @@ gulp.task('compress', function (cb) {
         }))
         .pipe(uglify())
         .pipe(gulp.dest('dist'));
+    
 });
 
-gulp.task('default', ['sprites', 'compress']);
+gulp.task('compresscss', function (cb) {
+    
+    gulp.src('src/**/*.css')
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist/'));
+});
+
+
+gulp.task('default', ['sprites', 'compress', 'compresscss']);
